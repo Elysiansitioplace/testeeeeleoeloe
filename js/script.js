a2feb980fb279c8b250940f4af4c0fa4d125670b
Elysiansitioplace <154612607+Elysiansitioplace@users.noreply.github.com>
2024-12-06 02:38:54 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const fundomenu = document.querySelector('#fundo-menumob');
+const cliquemenu = document.querySelector('#icon-menu');
+const fecharmenu = document.querySelector('#icon-fecharmenu')
+
+cliquemenu.addEventListener('click', ()=>{
+    alert('oii')
+});
